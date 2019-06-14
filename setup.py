2a03f0aeb4554d64478a61fe5b5e414fad7d67ed
adw62 <38112687+adw62@users.noreply.github.com>
2019-06-14 09:19:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='Fluorify',
+setup(name='LigCharOpt',
       version='0.1',
-      description='Automated Fluorine Scanning',
-      url='https://github.com/adw62/Fluorify',
+      description='Automated ligand charge optimisation',
+      url='https://github.com/adw62/Ligand_Charge_Optimiser',
       author='AW',
       author_email='None',
       license='None',
-      packages=['Fluorify'],
-      entry_points = {'console_scripts':['Fluorify = Fluorify.cli:main']})
+      packages=['LigCharOpt'],
+      entry_points = {'console_scripts':['LigCharOpt = LigCharOpt.cli:main']})
